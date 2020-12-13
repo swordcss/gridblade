57eb781afd74caf79e82392f5d0a71c6668beb9d
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-12-13 13:31:09 -0800

    chore: update to using dart sass instead of node sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const SwordCSS = require("gulp-sword");
 const postcss = require("gulp-postcss");
 const scss = require("gulp-sass");
 
+scss.compiler = require("dart-sass");
+
 const autoprefixer = require("autoprefixer");
 const presetEnv = require("postcss-preset-env");
 
